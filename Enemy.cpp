d5521eb63f36e3369c6cf3f1c7add55a378f704d
MatthewAntonioni1221 <mattantonioni745@gmail.com>
2024-11-05 16:06:16 -0500

    Update Enemy.cpp
    added comments

--- Enemy.cpp
+++ Enemy.cpp
@@ -6,17 +6,17 @@ using namespace std;
 class Enemy {
 private:
     std::string name;
-    const int aura;
+    const int aura; //this is the enemys health bar
 
 public:
-    Enemy(int aura) :name{"Nightmare"}, aura{aura} {
+    Enemy(int aura) :name{"Nightmare"}, aura{aura} { //delacrtion of a new enemy
     }
 
     virtual std::string toString( ) const {
-        return "Enemy " + name + " has aura " + std::to_string(aura);
+        return "Enemy " + name + " has aura " + std::to_string(aura); //this will display the name of the enemy and the "aura" or power level of the enemy
     }
 
-    int getAura() const {
+    int getAura() const { //this just returns the enemys aura
         return aura;
     }
 };
