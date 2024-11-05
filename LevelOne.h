cd313dc2af78e830e6e0e991edcb419bf7f5ef45
xuckless <xuckless@gmail.com>
2024-11-05 16:55:53 -0500

    HappyHappyHappy

--- LevelOne.h
+++ LevelOne.h
@@ -9,64 +9,65 @@
 
 class LevelOne : public Level {
 private:
-    bool isComplete = false; 
+    bool isComplete = false;
 
 public:
     LevelOne() = default;
 
     void proceed() override {
-        std::cout << "Hello, welcome to the Level 1" << std::endl;
+        std::cout << "Hello, welcome to Level 1" << std::endl;
 
-        std::cout<<"Whats your name?"<<std::endl;
+        std::cout << "What's your name?" << std::endl;
         std::string name;
+        std::cin >> name;
 
-        std::cin>>name;
-
-        Player *player = new Player(name, 20);
-        Enemy *enemy = new Enemy(150);
-
+        // Using smart pointers to manage memory
+        auto player = std::make_shared<Player>(name, 20);
+        auto enemy = std::make_shared<Enemy>(150);
 
         int choice;
 
-        std::cout<<"Do you want to look around? (1 (yes)/2 (no))"<<std::endl;
-        std::cin>>choice;
+        std::cout << "Do you want to look around? (1 (yes)/2 (no))" << std::endl;
+        std::cin >> choice;
 
         if (choice == 1) {
-            std::cout<<"You found a chest and a door"<<std::endl;
-            std::cout<<"Do you want to open the chest or the door? (1 (chest)/2 (door))"<<std::endl;
-            std::cin>>choice;
+            std::cout << "You found a chest and a door" << std::endl;
+            std::cout << "Do you want to open the chest or the door? (1 (chest)/2 (door))" << std::endl;
+            std::cin >> choice;
 
             if (choice == 1) {
-                std::cout<<"You got 75 aura"<<std::endl;
+                std::cout << "You got 75 aura" << std::endl;
                 player->increaseAura(75);
             } else if (choice == 2) {
-                std::cout<<"Welcome in the hallway"<<std::endl;
-                std::cout<<"Do you want to look around? (1 (yes)/2 (no))"<<std::endl;
-                std::cin>>choice;
-                 if (choice == 1) {
-                     std::cout<<"You found a chest and a door"<<std::endl;
-                     std::cout<<"Do you want to open the chest or the door? (1 (chest)/2 (door))"<<std::endl;
-                     cin>>choice;
+                std::cout << "Welcome to the hallway" << std::endl;
+                std::cout << "Do you want to look around? (1 (yes)/2 (no))" << std::endl;
+                std::cin >> choice;
 
-                     if (choice == 1) {
-                         std::cout<<"You got 80 aura"<<std::endl;
-                            player->increaseAura(80);
-                         std::cout <<"You have " << player->getAura() << " aura" << std::endl;
-                     } else {
-                         std::cout<<"There is a monster behind the door and he has 150 aura. Do you want to pass the door and fight him?"<<std::endl;
-                            std::cout<<"1 (yes)/2 (no)"<<std::endl;
-                         cin>>choice;
+                if (choice == 1) {
+                    std::cout << "You found a chest and a door" << std::endl;
+                    std::cout << "Do you want to open the chest or the door? (1 (chest)/2 (door))" << std::endl;
+                    std::cin >> choice;
 
-                         if (choice == 1) {
-                             std::cout<<"You have defeated the monster"<<std::endl;
-                             isComplete = true;
-                         } else {
-                             std::cout<<"You just standing there buddy"<<std::endl;
-                         }
-                     }
-                 }
+                    if (choice == 1) {
+                        std::cout << "You got 80 aura" << std::endl;
+                        player->increaseAura(80);
+                        std::cout << "You have " << player->getAura() << " aura" << std::endl;
+                    } else {
+                        std::cout << "There is a monster behind the door, and he has 150 aura. Do you want to pass the door and fight him?" << std::endl;
+                        std::cout << "1 (yes)/2 (no)" << std::endl;
+                        std::cin >> choice;
+
+                        if (choice == 1) {
+                            std::cout << "You have defeated the monster" << std::endl;
+                            isComplete = true;
+                        } else {
+                            std::cout << "You're just standing there, buddy" << std::endl;
+                        }
+                    }
+                }
             }
         }
+
         isComplete = true;
     }
 };
--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/eliebinamungu/Desktop/TextGme
+# Install script for directory: /Users/ali/cosc-projects/cpp/TextGme
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/Users/eliebinamungu/Desktop/TextGme/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/Users/ali/cosc-projects/cpp/TextGme/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
