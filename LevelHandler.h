aba8f8081d59926fc21ec9c0670e312669f3b5f1
MatthewAntonioni1221 <mattantonioni745@gmail.com>
2024-11-05 16:21:58 -0500

    Update LevelHandler.h
    added comments

--- LevelHandler.h
+++ LevelHandler.h
@@ -9,37 +9,37 @@ private:
     std::shared_ptr<Level> level;
     std::shared_ptr<LevelHandler> nextLevel;
 
-public:
+public: //this is a linked list that handles all the levels allowing us to have easier time calling them 
     LevelHandler(std::shared_ptr<Level> level, std::shared_ptr<LevelHandler> nextLevel = nullptr)
         : level(level), nextLevel(nextLevel) {}
 
-    std::shared_ptr<LevelHandler> getNextLevel() {
+    std::shared_ptr<LevelHandler> getNextLevel() { //this returns the next level
         return nextLevel;
     }
 
-    std::shared_ptr<Level> getLevel() {
+    std::shared_ptr<Level> getLevel() { //this returns the current level
         return level;
     }
 
-    void addLevelAfter(std::shared_ptr<Level> newLevel) {
+    void addLevelAfter(std::shared_ptr<Level> newLevel) { //this function adds a level after the head of the linked list 
         nextLevel = std::make_shared<LevelHandler>(newLevel, nextLevel);
     }
 
-    void addLevelInLast(std::shared_ptr<Level> newLevel) {
+    void addLevelInLast(std::shared_ptr<Level> newLevel) { //this function adds a level at the end of the linked list
         auto current = shared_from_this();
-        while (current->nextLevel) {
+        while (current->nextLevel) { 
             current = current->nextLevel;
         }
         current->addLevelAfter(newLevel);
     }
 
-    void removeLevelAfter() {
+    void removeLevelAfter() { //this function removes a level however this function will most likely never be used
         if (nextLevel) {
             nextLevel = nextLevel->getNextLevel();
         }
     }
 
-    void removeFromLast() {
+    void removeFromLast() { //this function is the same case as the one above
         auto current = shared_from_this();
         while (current->nextLevel && current->nextLevel->nextLevel) {
             current = current->nextLevel;
