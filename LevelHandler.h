19c7eb1d6ab81a81b027851bf25fefa873020b18
MatthewAntonioni1221 <mattantonioni745@gmail.com>
2024-11-18 11:54:19 -0800

    Update LevelHandler.h

--- LevelHandler.h
+++ LevelHandler.h
@@ -4,21 +4,21 @@
 
 #include "Level.h"
 
-class LevelHandler {
+class LevelHandler { //this adds the elements needed for a linked list
 private:
-    Level* level;
-    LevelHandler* nextLevel;
+    Level* level; //this is the pointer for the current level
+    LevelHandler* nextLevel; //this allows you to search through the list
 
-public:
+public: //these fucntions allow the utlilty of the linked list
     LevelHandler(Level* level, LevelHandler* nextLevel = nullptr);
     ~LevelHandler();
 
     LevelHandler* getNextLevel();
-    Level* getLevel();
-    void addLevelAfter(Level* level);
-    void addLevelInLast(Level* level);
-    void removeLevelAfter();
-    void removeFromLast();
+    Level* getLevel(); //this returns the current level
+    void addLevelAfter(Level* level); //this adds a node to a list after the head of the list
+    void addLevelInLast(Level* level); //this adds one at the end of the list
+    void removeLevelAfter(); //this removes a node after the head of the list
+    void removeFromLast(); //this removes the last node of the list
 };
 
 #endif // LEVELHANDLER_H
