631a1c690fcf4bc9ed494e34d8d466319d243c73
MatthewAntonioni1221 <mattantonioni745@gmail.com>
2024-11-18 11:23:05 -0800

    Update Enemy.cpp

--- Enemy.cpp
+++ Enemy.cpp
@@ -6,29 +6,29 @@
 #include <ctime>
 
 Enemy::Enemy(const std::string& name, int initialHealth)
-    : name(name), health(initialHealth) {}
+    : name(name), health(initialHealth) {} //this intizles the enemys name and health
 
-std::string Enemy::getName() const {
+std::string Enemy::getName() const { //this returns the enemys name
     return name;
 }
 
-int Enemy::getHealth() const {
+int Enemy::getHealth() const { //this returns the enemys health
     return health;
 }
 
-void Enemy::decreaseHealth(int amount) {
-    health -= amount;
+void Enemy::decreaseHealth(int amount) { //this decreases the health of the enemy when called
+    health -= amount; //this will decrase the enemys health based on amount
     std::cout << name << " took " << amount << " damage!" << std::endl;
     std::cout << name << "'s current health: " << health << std::endl;
 }
 
-void Enemy::increaseHealth(int amount) {
-    health += amount;
+void Enemy::increaseHealth(int amount) { //this addsa health to the enemy
+    health += amount; //this adds health to the enemy
     std::cout << name << " healed by " << amount << " points!" << std::endl;
     std::cout << name << "'s current health: " << health << std::endl;
 }
 
-bool Enemy::isDead() const {
+bool Enemy::isDead() const { //this will return if the enemys health is equal to zero
     return health <= 0;
 }
 
